1ca289a9f80d98d168bd8dea1bd55c329adcd0b6
g4bri0s <g.cristianods@gmail.com>
2023-03-31 17:01:23 -0300

    "Commit the first pages of Sleeping Car Library"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButomComponent } from './components/butom/butom.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BooksComponent } from './pages/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ButomComponent,
+    HomeComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
